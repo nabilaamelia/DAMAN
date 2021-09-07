8574a250278ed4869213dc0ebf38c0a126b1bd8a
Alifpogeng <pogeng45ti@gmail.com>
2021-09-07 11:15:21 +0700

    tampilan edit data

--- application/views/admin/data_pkl.php
+++ application/views/admin/data_pkl.php
@@ -30,9 +30,80 @@
         
         <td>
           <!-- <button class="btn btn-sm btn-primary mb-4" data-toggle="modal" data-target="#edit_data_pkl"><i class="fas fa-plus fa-sm"></i> Tambah Barang</button> -->
-          <?php echo anchor('admin/data_barang/edit/'.$pkl->id_data_pkl, '<button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#edit_data_pkl"><i class="fa fa-edit"></i></button>') ?>
+          
+          <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#edit_data_pkl<?php echo $pkl->id_data_pkl; ?>"><i class="fa fa-edit"></i></button>
+
+          <!-- Ubah Data -->
+          <div class="modal fade" id="edit_data_pkl<?php echo $pkl->id_data_pkl; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">EDIT DATA PKL</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                  <form action="<?php echo base_url().'admin/edit_data_pkl'; ?>" method="post" enctype="multipart/form-data" >
+
+                    <div class="form-group">
+                      <label>Nama</label>
+                      <input type="text" name="nama_pkl" class="form-control" value="<?php echo $pkl->nama_pkl ?>">
+                    </div>
+
+                    <div class="form-group">
+                      <label>Asal Sekolah</label>
+                      <input type="text" name="asal_sekolah" class="form-control" value="<?php echo $pkl->asal_sekolah ?>">
+                    </div>
+
+                    <div class="form-group">
+                      <label>No Hp</label>
+                      <input type="number" name="no_hp" class="form-control" value="<?php echo $pkl->no_hp ?>">
+                    </div>
+
+                    <div class="form-group">
+                      <label>Email</label>
+                      <input type="email" name="email" class="form-control" value="<?php echo $pkl->email ?>">
+                    </div>
+
+                    <div class="form-group">
+                      <label>Kota</label>
+                      <input type="text" name="kota" class="form-control" value="<?php echo $pkl->kota ?>">
+                    </div>
+
+                    <div class="form-group">
+                      <label>Alamat</label>
+                      <textarea class="form-control" name="alamat" id="" cols="30" rows="10"><?php echo $pkl->alamat ?></textarea>
+                    </div>
+
+                    <div class="form-group">
+                      <label>Tanggal Mulai</label>
+                      <input type="date" name="tgl_mulai" class="form-control" value="<?php echo $pkl->tgl_mulai ?>">
+                    </div>
+
+                    <div class="form-group">
+                      <label>Tanggal Selesai</label>
+                      <input type="date" name="tgl_selesai" class="form-control" value="<?php echo $pkl->tgl_selesai ?>">
+                    </div>
+
+                    <div class="form-group">
+                      <label>foto</label><br>
+                      <input type="file" name="foto" class="form-control">
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                  <button type="submit" class="btn btn-primary">Simpan</button>
+                  <?php echo anchor('admin/data_pkl',
+                    '<div class="btn  btn-success">Close</div>') ?>
+                </div>
+
+                </form>
+
+              </div>
+            </div>
+          </div>
+        <!-- Akhir edit data -->
             
-          </td> 
+        </td> 
         <td><?php echo anchor('admin/data_barang/hapus/' .$pkl->id_data_pkl, '<div class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></div>') ?></td>
         
       </tr>
@@ -45,8 +116,8 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">FORM INPUT PRODUK</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <h5 class="modal-title" id="exampleModalLabel">FORM DATA PKL</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
         <form action="<?php echo base_url().'admin/tambah_data_pkl'; ?>" method="post" enctype="multipart/form-data" >
@@ -68,7 +139,7 @@
 
           <div class="form-group">
             <label>Email</label>
-            <input type="text" name="email" class="form-control">
+            <input type="email" name="email" class="form-control">
           </div>
 
           <div class="form-group">
