478a52fa8cc6558ef3191d7fa5894cd7d243dfcf
yenipp <78332283+yenipp@users.noreply.github.com>
2021-09-16 15:27:28 +0700

    yeni

--- application/views/peserta_pkl.php
+++ application/views/peserta_pkl.php
@@ -1,18 +1,23 @@
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!---jss---->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
     <div class='animatebox animate fadeInRightBig panel'>
         <div style='width: 100px; float: left; margin-right: 10px; margin-top: 10px'>
             <img title='' src='http://pegawai.pnm.ac.id/assets/file/pegawai/foto/20170908104836.JPG' style='border-radius: 5px;'/>
         </div>
-        <div>
-            <table class="table table-bordered">
+        <div class="container">
+        <h1 class="text-center">Daftar Peserta PKL</h1>
+            <table table class="table table-bordered table-striped table-hover">
                 <tr>
-                    <td>NO</td>
+                    <td>No</td>
                     <td>Nama</td>
-                    <td>ASAL SEKOLAH</td>
-                    <td>NO HP</td>
-                    <td>EMAIL</td>
-                    <td>KOTA</td>
-                    <td>MULAI</td>
-                    <td>SELESAI</td>
+                    <td>Asal Sekolah</td>
+                    <td>No HP</td>
+                    <td>Email</td>
+                    <td>Kota</td>
+                    <td>Tanggal Mulai</td>
+                    <td>Tanggal Selesai</td>
                 </tr>
 
                 <?php
