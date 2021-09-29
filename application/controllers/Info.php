712f3e9b7b955017ef85267e9733ed6d6e5820c8
Alifpogeng <pogeng45ti@gmail.com>
2021-09-29 11:05:23 +0700

    gas

---
+++ application/controllers/Info.php
@@ -0,0 +1,15 @@
+<?php
+
+class Info extends CI_Controller{
+
+	public function index()
+	{
+		$data['infodaman'] = $this->Model_data->tampil_info_daman()->result();
+		$this->load->view('templates/header');
+		$this->load->view('templates/navbar');
+		$this->load->view('info', $data);
+		$this->load->view('templates/footer');
+	}
+}
+
+?>
