3ffdf1186d5b64a13c6aba20e48d462038c4c3be
nabilaamelia <nabilaamelia46@gmail.com>
2021-10-26 13:03:18 +0700

    UBAH

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -68,7 +68,7 @@
     <div class="container">
         <div class="row">
             <div class="text-center w-lg-75 m-auto pb-4">
-                <h12 >STRUKTUR KEPENGURUSAN</h1>
+                <h12 >STRUKTUR ORGANISASI</h1>
                     <h2 class="py-2">DIVISI DATA MANAGEMENT</h2>
                     
                 </div>
